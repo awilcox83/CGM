667eac3c5da043ba88976075be7f7fc4e8cdca37
Alex Wilcox <41028740+awilcox83@users.noreply.github.com>
2019-04-20 07:09:10 -0700

    Update CGM.py

--- CGM.py
+++ CGM.py
@@ -150,9 +150,6 @@ def readSplitter(SAMfile):
 			g.write(line)
 		else:
 			x = line.split()
-			# TODO hard to split reads cigars with deletions right now, so just write them as-is
-			if 'D' in x[5]:
-				g.write(line)
 			# check to see if read spans break 
 			elif int(x[3]) + len(x[9]) -1 > refLength:
 				#Call function
